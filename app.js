10f08ed812a685936618d49486ce05b79090ffc3
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-01-09 17:19:00 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var btnTransalte = document.querySelector("#btn-translate");
 
+var textArea = document.querySelector("#textArea");
+
+
 btnTransalte.addEventListener("click", function clickHandler() {
   console.log("clicked");
+  console.log(textArea.value);
 });
+
