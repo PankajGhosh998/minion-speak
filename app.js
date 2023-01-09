4996a10c76e764b4bf890b55ad70fc06ae17097b
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-01-09 18:11:16 +0530

    add output area

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var btnTransalte = document.querySelector("#btn-translate");
 
 var textArea = document.querySelector("#textArea");
-
+var outputArea = document.querySelector("#output");
 
 btnTransalte.addEventListener("click", function clickHandler() {
   console.log("clicked");
   console.log(textArea.value);
-});
 
+  outputArea.innerText = "banana " + textArea.value;
+});
